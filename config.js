362ee9736f9bd14bfe0bdea059419092011fd4e1
Jason Bettyas <jbettyas@gmail.com>
2019-04-18 14:11:01 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '568850496944912',
+        clientSecret: '87bab68814b36f4e286618af38b5a1cf'
+    }
 }
